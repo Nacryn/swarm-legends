79f2d67e568dd6cf3c2e026dbf432eeb7070a75a
Mikaël <lorack@gmail.com>
2018-07-30 17:50:08 +0200

    Reinitialization with PWA Starter Kit, and on we go with Polymer 3

--- src/client/app/components/timeline/post.ts
+++
@@ -1,5 +0,0 @@
-export interface Post {
-  id: number;
-  date: string;
-  name: string;
-}
--- src/client/app/shared/mock-characters.ts
+++
@@ -1,8 +0,0 @@
-import {Character} from './character.ts';
-
-export var CHARACTERS: Character[] = [
-    {'id': 1, 'name': 'A'},
-    {'id': 2, 'name': 'B'},
-    {'id': 3, 'name': 'C'},
-    {'id': 4, 'name': 'D'},
-];
